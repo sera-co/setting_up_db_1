bf2ca53049c890b5a7bf9dcf20611e462cb99ddd
unknown <sera.shine@kalvium.community>
2025-01-06 12:05:37 +0530

    changes made

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
+app.use(express.json())
 
 app.use(express.static('static'));
 
+mongoose
+.connect(process.env.DB_URL)
+.then(()=>console.log('successfully connected to database'))
+.catch((err)=>console.log("Error connecting",err))
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
